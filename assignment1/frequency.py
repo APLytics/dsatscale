2979deb8b27d23d49630fdc76ee220bf30b48f41
angelpl <Angel Perez Lemonche>
2018-09-27 22:51:39 -0400

    Updated and functional term_sentiment.py and frequency.py

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -11,16 +11,21 @@ def compute_term_frequency(file):
             if 'text' in tweet.keys() :
                 text = tweet['text'].split(' ')
                 for word in text:
+                    if not word.isalnum():
+                        continue
                     try:
                         new_term[word] += 1
                     except:
                         new_term[word] = 1
 
+    N = float(sum(new_term.values()))
+
+    new_term = { k: v/N for k, v in new_term.items()}
     return new_term
 
 def print_new_term_freq(dic):
     for k, v in dic.items():
-        print(k,v)
+        print k, v
 
 def main():
     
